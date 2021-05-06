996873f23c551758c2d8ea4e8d370c7178e2d952
a01750147 <A01750147@itesm.mx>
2021-05-06 12:55:48 -0500

    Color random de la serpiente y la comida

--- snake.py
+++ snake.py
@@ -1,22 +1,40 @@
+#Codigo modificado por:
+#Autor: Arturo Cuauhtémoc Ruiz Leyva
+#Autor: Andrea Vianey Díaz Álvarez
+
+"""Se importan las librerías que se usan en el código"""
+import random
 from turtle import *
 from random import randrange
 from freegames import square, vector
 
+
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
+
 
 def change(x, y):
     "Change snake direction."
     aim.x = x
     aim.y = y
 
+
 def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
 
+
+color = ['orange', 'blue','black', 'green', 'grey']
+colorSerpiente = random.choice(color)
+colorComida = random.choice(color)
+
+while colorComida == colorSerpiente:
+    colorComida = random.choice(color)
+
 def move():
     "Move snake forward one segment."
+    
     head = snake[-1].copy()
     head.move(aim)
 
@@ -27,6 +45,7 @@ def move():
 
     snake.append(head)
 
+
     if head == food:
         print('Snake:', len(snake))
         food.x = randrange(-15, 15) * 10
@@ -35,11 +54,12 @@ def move():
         snake.pop(0)
 
     clear()
+    
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9, colorSerpiente)
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, colorComida)
     update()
     ontimer(move, 100)
 
@@ -47,9 +67,13 @@ setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 listen()
+
+#Define la direccion de la serpiente
 onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
+
 move()
+
 done()
